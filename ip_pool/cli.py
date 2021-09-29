128a789d8eebae409849767b738eafa32280dd67
Zachary Cutlip <uid000@gmail.com>
2021-09-28 19:12:14 -0700

    main() parses args an instantiates an ip address pool

--- ip_pool/cli.py
+++ ip_pool/cli.py
@@ -1,19 +1,24 @@
-import sys
+from argparse import ArgumentParser
 
-from .version import IPPoolAbout as About
+from .ip_pool import IPAddressPool
 
 
-def main():
-    argv = sys.argv
-    print("IP Address Pool main()")
-    print("%s" % About())
-    if argv:
-        print("args:")
-        for arg in argv:
-            print(arg)
-    else:
-        print("No args provided")
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument("pool_db_json", help="Path to ip pool database json file")
+    parser.add_argument("--initialize", metavar="cidr_address", help="Initialize IP pool with the provided CIDR address/netmask")
+    parsed = parser.parse_args()
+    return parsed
+
+
+def ip_pool_main():
+    options = parse_args()
+    pool = IPAddressPool(options.pool_db_json)
+    if options.initialize:
+        pool.initialize(options.initialize)
+    for addr in pool.addresses:
+        print(addr)
 
 
 if __name__ == "__main__":
-    exit(main())
+    exit(ip_pool_main())
